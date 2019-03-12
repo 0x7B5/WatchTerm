0f5222afe908f578d0e4a5c0a53915bdac8834e1
vlad-munteanu <vladcmunteanu@gmail.com>
2019-03-12 11:33:57 -0400

    first commit

---
+++ WatchTerm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WatchTerm
+//
+//  Created by Vlad Munteanu on 3/12/19.
+//  Copyright © 2019 Vlad Munteanu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
